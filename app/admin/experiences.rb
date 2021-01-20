7d7e86e02ce77db2b231f4f0d8ef3d9a9e402140
kajal1107 <kajalyuvasoft298@gmail.com>
2021-01-20 12:21:23 +0530

    add header in view on each page and edit form in admin side

--- app/admin/experiences.rb
+++ app/admin/experiences.rb
@@ -1,5 +1,15 @@
 ActiveAdmin.register Experience do
-     menu parent: "Profile"
+  form title: 'Interview Schedule Details' do |f|
+    inputs 'Details' do
+      input :status, :as => :select, collection: (['PENDING', 'ON HOLD', 'NOT RELEVANT', 'IN PROCESS', 'SCHEDULED', 'DISQUALIFIED', 'REJECTED','FIRST ROUND', 'SECOND ROUND','INTERVIEW ROUND','MACHINE TEST', 'HR ROUND', 'SELECTED', 'CONFIRMED', 'JOINED'])
+      input :interview_type, :as => :select, collection: (['TELEPHONE INTERVIEW', 'FACE TO FACE INTERVIEW', 'SKYPE INTERVIEW'])
+      input :interviewer, :as => :select, collection: (['Prateek Singh', 'Hemant Gupta', 'Anand Rajput', 'Sandeep Singh', 'Rohit Yadav', 'Nirmala Malviya', 'Gourav Lele', 'Manjari Mishra', 'Swati Mishra', 'Hitesh Kanwar' ])
+      input :schedule_at
+      input :admin_note
+    end
+    actions
+  end
+  #menu parent: "Profile"
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -10,7 +20,7 @@ ActiveAdmin.register Experience do
   # or
   #
   permit_params do
-    permitted = [:name, :email, :gender, :contact, :experience, :highest_qualification, :branch, :apply_for, :current_company, :current_location, :resume, :note, :status, :interview_type, :interviewer, :admin_note, :current_ctc, :expected_ctc, :expected_negotiable, :notice_period, :notice_period_negotiable, :schedule_at]
+    permitted = [:status, :interview_type, :interviewer, :admin_note, :schedule_at]
     permitted << :other if params[:action] == 'create' && current_user.admin?
     permitted
   end
--- app/admin/freshers.rb
+++ app/admin/freshers.rb
@@ -1,5 +1,14 @@
 ActiveAdmin.register Fresher do
-
+  form title: 'Interview Schedule Details' do |f|
+    inputs 'Details' do
+      input :status, :as => :select, collection: (['PENDING', 'ON HOLD', 'NOT RELEVANT', 'IN PROCESS', 'SCHEDULED', 'DISQUALIFIED', 'REJECTED','FIRST ROUND', 'SECOND ROUND','INTERVIEW ROUND','MACHINE TEST', 'HR ROUND', 'SELECTED', 'CONFIRMED', 'JOINED'])
+      input :interview_type, :as => :select, collection: (['TELEPHONE INTERVIEW', 'FACE TO FACE INTERVIEW', 'SKYPE INTERVIEW'])
+      input :interviewer, :as => :select, collection: (['Prateek Singh', 'Hemant Gupta', 'Anand Rajput', 'Sandeep Singh', 'Rohit Yadav', 'Nirmala Malviya', 'Gourav Lele', 'Manjari Mishra', 'Swati Mishra', 'Hitesh Kanwar' ])
+      input :schedule_at
+      input :admin_note
+    end
+    actions
+  end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -10,7 +19,7 @@ ActiveAdmin.register Fresher do
   # or
   #
   permit_params do
-    permitted = [:name, :email, :gender, :contact, :highest_qualification, :branch, :college, :passout_year, :current_location, :resume, :note, :status, :interview_type, :schedule_at, :interviewer, :admin_note]
+    permitted = [:status, :interview_type, :schedule_at, :interviewer, :admin_note]
     permitted << :other if params[:action] == 'create' && current_user.admin?
     permitted
   end
