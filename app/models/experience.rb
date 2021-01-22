0252f55e5f82b60490900a920d7022ec8e00cbed
rails-system <shivani.yuvasoft290@gmail.com>
2021-01-22 18:47:57 +0530

    #Fixed UI

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,4 +2,5 @@ class Experience < ApplicationRecord
 	validates :name,:experience,:branch,:current_company,:current_location,:resume,:current_ctc,:expected_ctc,:notice_period, presence: true
 	validates :email, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 	validates :contact, length: { minimum: 10 }
+  GENDER = ['MALE', 'FEMALE']
 end
--- app/models/fresher.rb
+++ app/models/fresher.rb
@@ -2,4 +2,5 @@ class Fresher < ApplicationRecord
 	validates :name,:highest_qualification,:branch,:college,:passout_year,:current_location,:resume, presence: true
 	validates :email, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 	validates :contact, length: { minimum: 10 }
+  GENDER = ['MALE', 'FEMALE']
 end
