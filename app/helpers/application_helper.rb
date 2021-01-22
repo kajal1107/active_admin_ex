10e54b2cab3730bf21cb3fff230341c404ee5004
rails-system <shivani.yuvasoft290@gmail.com>
2021-01-22 19:15:59 +0530

    #Fixed Validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,10 @@
 module ApplicationHelper
-	def show_errors(object, field_name)
-  	if object.errors.full_messages.any?
-     	if !object.errors.messages[field_name].blank?
-      	if field_name == :campus_drive_id
-        	"appear_in" + " " + object.errors.messages[field_name].join(", ")
-      	else
-        	field_name.to_s.capitalize + " " + object.errors.messages[field_name].join(", ")
-       	end
-     	end
-   	end
- 	end
+  def show_errors(object, field_name)
+    if object.errors.full_messages.any?
+      if !object.errors.messages[field_name].blank?
+        field_name.to_s.titleize + " " + object.errors.messages[field_name].join(", ").titleize
+      end
+    end
+  end 
 end
   
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,5 @@
 class Experience < ApplicationRecord
-	validates :name,:experience,:branch,:current_company,:current_location,:resume,:current_ctc,:expected_ctc,:notice_period, presence: true
+	validates :name,:experience,:branch,:current_company,:current_location,:resume,:current_ctc,:expected_ctc,:notice_period,:notice_period_negotiable, presence: true
 	validates :email, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 	validates :contact, length: { minimum: 10 }
   GENDER = ['MALE', 'FEMALE']
